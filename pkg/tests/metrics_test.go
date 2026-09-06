13f433837b9645b571d3a5895f36124e08f65abf
0z5a <dezhen.lu@student.uni-tuebingen.de>
2026-09-06 21:49:39 +0800

    test(metrics): cover default and explicit cache dtypes
    Check omitted, explicit auto and TurboQuant cache dtype exposition without changing the billion-block configuration assertion.
    
    Signed-off-by: 0z5a <dezhen.lu@student.uni-tuebingen.de>

--- pkg/tests/metrics_test.go
+++ pkg/tests/metrics_test.go
@@ -916,24 +916,32 @@ var _ = Describe("Simulator metrics", Ordered, func() {
 			Expect(*hits).To(BeNumerically("<=", *queries))
 		})
 
-		It("Should send correct kv cache config metrics", func() {
+		DescribeTable("Should send correct kv cache config metrics", func(cacheDType, expectedDType string) {
 			ctx := context.TODO()
 			args := []string{"cmd", "--model", common.QwenModelName, "--mode", common.ModeRandom,
-				"--kv-cache-size", "1000000000", "--block-size", "8",
-				"--kv-cache-dtype", "turboquant_4bit_nc"}
+				"--kv-cache-size", "1000000000", "--block-size", "8"}
+			if cacheDType != "" {
+				args = append(args, "--kv-cache-dtype", cacheDType)
+			}
 
 			client, err := startServerWithArgsAndEnv(ctx, common.ModeRandom, args, map[string]string{"POD_IP": "localhost"})
 			Expect(err).NotTo(HaveOccurred())
 
 			metricsResp, err := client.Get(metricsUrl)
 			Expect(err).NotTo(HaveOccurred())
+			defer func() { _ = metricsResp.Body.Close() }()
 			Expect(metricsResp.StatusCode).To(Equal(http.StatusOK))
 
 			data, err := io.ReadAll(metricsResp.Body)
 			Expect(err).NotTo(HaveOccurred())
 			metrics := string(data)
-			Expect(metrics).To(ContainSubstring("vllm:cache_config_info{block_size=\"8\",cache_dtype=\"turboquant_4bit_nc\",num_cpu_blocks=\"0\",num_gpu_blocks=\"1000000000\"} 1"))
-		})
+			Expect(metrics).To(ContainSubstring(fmt.Sprintf(
+				"vllm:cache_config_info{block_size=\"8\",cache_dtype=%q,num_cpu_blocks=\"0\",num_gpu_blocks=\"1000000000\"} 1", expectedDType)))
+		},
+			Entry("default dtype", "", "auto"),
+			Entry("explicit auto dtype", "auto", "auto"),
+			Entry("TurboQuant dtype", "turboquant_4bit_nc", "turboquant_4bit_nc"),
+		)
 	})
 
 	Context("single request latency metrics", func() {
